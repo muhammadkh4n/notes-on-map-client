b946c96d17bceb8744c887367a332357acb66fa0
Muhammad Khan <muhammadkh4n@gmail.com>
2018-11-25 17:24:03 +0500

    app bar added

--- src/layout/header/Header.js
+++ src/layout/header/Header.js
@@ -1,11 +1,36 @@
 import React from 'react';
+import Proptypes from 'prop-types';
+import {
+  AppBar,
+  Toolbar,
+  Button,
+  withStyles
+} from '@material-ui/core';
+import { Link, withRouter } from 'react-router-dom';
 
-const Header = () => {
+import styles from './styles';
+
+const Header = ({ classes }) => {
   return (
-    <header className="App-header">
-      Header
+    <header className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <nav className={classes.grow}>
+            <Link to="/" className={classes.link}>
+              <Button
+                color="inherit">Home</Button>
+            </Link>
+          </nav>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
     </header>
   );
 };
 
-export default Header;
+Header.propTypes = {
+  classes: Proptypes.object.isRequired
+};
+
+const composeStyles = withStyles(styles);
+export default composeStyles(withRouter(Header));
---
+++ src/layout/header/styles.js
@@ -0,0 +1,14 @@
+import { colors } from '@material-ui/core';
+
+export default {
+  root: {
+    flexGrow: 1,
+  },
+  grow: {
+    flexGrow: 1
+  },
+  link: {
+    'color': colors.grey[100],
+    'text-decoration': 'none'
+  }
+}
